cad72cea9fc2078d7cc7d562c7053c283fa6355c
SiddDevZ <epic.siddarth@gmail.com>
2024-06-09 17:29:32 +0530

    fixing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: [],
   theme: {
-    extend: {
-      backgroundColor: {
-        'github': "rgb(39, 39, 42)"
-      },
-      screens: {
-        'xss': '220px', 
-        'xs': '480px',
-        '3xl': '1920px',
-      },
-      fontFamily: {
-        pop: ["Poppins", "sans-serif"]
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
