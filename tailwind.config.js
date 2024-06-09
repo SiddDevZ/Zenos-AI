bdeb697f3732252fde1a0ba00b57cf4c3a4c2ad4
SiddDevZ <epic.siddarth@gmail.com>
2024-06-09 18:08:42 +0530

    Tailwind Bug Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/Pages/**/*.{html,jsx, js, tsx}',
-    './src/components/**/*.{html,jsx, js, tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
