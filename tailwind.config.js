59c7da93ed43ce1b831701a7453aaaad652e15a2
SiddDevZ <epic.siddarth@gmail.com>
2024-06-09 17:35:17 +0530

    Fixing Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'github': "rgb(39, 39, 42)"
+      },
+      screens: {
+        'xss': '220px', 
+        'xs': '480px',
+        '3xl': '1920px',
+      },
+      fontFamily: {
+        pop: ["Poppins", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
