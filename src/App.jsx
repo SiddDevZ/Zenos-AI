fa3df31b4723a34ee08f6fc98bc708589c45fe60
SiddDevZ <epic.siddarth@gmail.com>
2024-06-12 07:58:25 +0530

    route testing 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Chat';
 import SignIn from './Pages/Sign_in/SignIn';
 import SignUp from './Pages/Sign_up/SignUp';
-import Chat from './Pages/Chat/Home';
+import AIChat from './Pages/Chat/Home';
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/aichat" element={<Chat />} />
+        <Route path="/aichat" element={<AIChat />} />
         <Route path="/sign-up" element={<SignUp />} />
         {/* <Route path="/contact" element={<Home />} */}
       </Routes>
