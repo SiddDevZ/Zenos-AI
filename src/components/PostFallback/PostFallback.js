dd39f03667d59ba041a212b26103c0c188e9e3dc
SiddDevZ <epic.siddarth@gmail.com>
2024-08-11 19:16:48 +0530

    APIs Testing

--- src/components/PostFallback/PostFallback.js
+++ src/components/PostFallback/PostFallback.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 import config from '../../../config.json';
 
+let apiPriority = [
+    `${config.url1}/candlyn/`,
+    `${config.url2}/candlyn/`,
+    // `${config.url3}/candlyn/`
+];
+
 const postWithFallback = async (endpoint, data) => {
-    const urls = [
-        `${config.url1}/candlyn/${endpoint}`,
-        `${config.url2}/candlyn/${endpoint}`,
-        // `${config.url3}/candlyn/${endpoint}`
-    ];
-    for (const url of urls) {
+    for (let i = 0; i < apiPriority.length; i++) {
+        const url = apiPriority[i] + endpoint;
         try {
             const result = await axios.post(url, data, { timeout: 2500 });
             return result;
         } catch (error) {
             console.log(`Failed to fetch from ${url}: `, error.message);
+            // Move the failed URL to the end of the priority list
+            apiPriority.push(apiPriority.splice(i, 1)[0]);
+            i--; // Adjust index to account for the moved element
         }
     }
     throw new Error("All API requests failed");
